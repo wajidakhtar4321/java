8ba1d039bfd85b072612c266cdbdff14d2b577b4
Wajid Akhtar <142623371+wajidakhtar4321@users.noreply.github.com>
2023-08-19 22:08:40 +0530

    Add files via upload

---
+++ airthmeticoperator.java
@@ -0,0 +1,39 @@
+
+public class airthmeticoperator {
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 5;
+        System.out.println(a + b); // add
+        System.out.println(a - b); // subtract
+        System.out.println(a * b); // multiply
+        System.out.println(a / b); // divide
+        System.out.println(a % b); // modoule
+    }
+}
+
+/*
+ * import java.util.*; // value of A = value of B
+ * 
+ * public class airthmeticoperator {
+ * public static void main(String args[]) {
+ * Scanner sc = new Scanner(System.in);
+ * int a = sc.nextInt();
+ * int b = a;
+ * System.out.println("value os a is " + a);
+ * System.out.println("value of b is " + b);
+ * }
+ * }
+ */
+/*
+ * import java.util.*; //value of A = value of B
+ * 
+ * public class airthmeticoperator {
+ * public static void main(String args[]) {
+ * Scanner sc = new Scanner(System.in);
+ * int a = 5;
+ * int b = a;
+ * System.out.println("value of a is " + a);
+ * System.out.println("value of b is " + b);
+ * }
+ * }
+ */
